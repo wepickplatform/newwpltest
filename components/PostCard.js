d1c725c44ce453322f0cb7f93b31bece6f3843cc
wepickplatform <sanghyeon@wepick.kr>
2025-05-02 14:54:36 +0900

    Update PostCard.js

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,3 +1,4 @@
+// components/PostCard.js
 import Link from 'next/link';
 import Image from 'next/image';
 import moment from 'moment';
@@ -5,47 +6,49 @@ import 'moment/locale/ko';
 import styles from '../styles/PostCard.module.css';
 
 export default function PostCard({ post }) {
-  const featuredMedia = post._embedded?.['wp:featuredmedia']?.[0];
-  const author = post._embedded?.['author']?.[0];
-  
   moment.locale('ko');
   const formattedDate = moment(post.date).format('YYYY년 MM월 DD일');
   
+  const featuredMedia = post._embedded?.['wp:featuredmedia']?.[0];
+  const author = post._embedded?.['author']?.[0];
+  
   return (
     <div className={styles.card}>
-      {featuredMedia && featuredMedia.source_url && (
-        <div className={styles.imageContainer}>
-          <Image
-            src={featuredMedia.source_url}
-            alt={post.title.rendered}
-            layout="fill"
-            objectFit="cover"
-            className={styles.image}
-          />
-        </div>
-      )}
-      
-      <div className={styles.content}>
-        <h2>
-          <Link href={`/post/${post.slug}`}>
-            <a dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
-          </Link>
-        </h2>
-        
-        <div className={styles.meta}>
-          {author && <span>작성자: {author.name}</span>}
-          <span>작성일: {formattedDate}</span>
-        </div>
-        
-        <div 
-          className={styles.excerpt}
-          dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-        />
-        
-        <Link href={`/post/${post.slug}`}>
-          <a className={styles.readMore}>자세히 보기</a>
-        </Link>
-      </div>
+      <Link href={`/post/${post.slug}`}>
+        <a className={styles.cardLink}>
+          {featuredMedia && featuredMedia.source_url && (
+            <div className={styles.imageContainer}>
+              <Image
+                src={featuredMedia.source_url}
+                alt={post.title.rendered}
+                width={300}
+                height={200}
+                layout="responsive"
+                className={styles.image}
+              />
+            </div>
+          )}
+          
+          <div className={styles.cardContent}>
+            <h3 
+              className={styles.cardTitle}
+              dangerouslySetInnerHTML={{ __html: post.title.rendered }} 
+            />
+            
+            <div className={styles.cardMeta}>
+              {author && <span>작성자: {author.name}</span>}
+              <span>작성일: {formattedDate}</span>
+            </div>
+            
+            <div 
+              className={styles.cardExcerpt}
+              dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+            />
+            
+            <span className={styles.readMore}>자세히 보기</span>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 }
