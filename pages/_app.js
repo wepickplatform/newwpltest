c9036e7e582cc520bb96c25d1be2b23ca6185f39
wepickplatform <sanghyeon@wepick.kr>
2025-05-02 16:42:03 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 // pages/_app.js
+// pages/_app.js
 import '../styles/globals.css';
+import { AuthProvider } from '../lib/auth';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
