fb4e12fe1e759a2d1d9a6c2e40bf8d97d01325d9
wepickplatform <sanghyeon@wepick.kr>
2025-05-02 10:43:02 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['letter.wepick.kr'], // 워드프레스 사이트 도메인 추가
-      formats: ['image/avif', 'image/webp'],
-    },
-    i18n: {
-      locales: ['ko'],
-      defaultLocale: 'ko',
-    }
-  };
+  reactStrictMode: true,
+  images: {
+    domains: ['letter.wepick.kr'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  i18n: {
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
+  // 추가: 서버 사이드 렌더링 활성화
+  trailingSlash: true
+}
